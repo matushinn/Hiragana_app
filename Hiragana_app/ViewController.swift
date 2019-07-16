24b7842a6cf7a7af24cd797c9f68790b65ee324e
shotaro-ooe <shoooe97@gmail.com>
2019-07-16 22:19:37 +0900

    first commit

---
+++ Hiragana_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hiragana_app
+//
+//  Created by 大江祥太郎 on 2019/07/16.
+//  Copyright © 2019 shotaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
